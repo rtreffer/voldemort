7c00b9a59dc42dfd8bb74ec0c421c60ed72e292d
Rene Treffer <treffer@localhost.(none)>
2009-05-26 08:50:18 +0200

    Add some initiali javadoc

--- src/java/voldemort/store/kdtree/KDTreeIterator.java
+++ src/java/voldemort/store/kdtree/KDTreeIterator.java
@@ -7,20 +7,55 @@ import voldemort.utils.ClosableIterator;
 import voldemort.utils.Pair;
 import voldemort.versioning.Versioned;
 
+/**
+ * Iterate over a kd tree.
+ */
 public class KDTreeIterator implements ClosableIterator<Pair<ByteArray, Versioned<byte[]>>> {
 
+    /**
+     * The current position (kd node).
+     */
     private KDNode<List<Versioned<byte[]>>> position;
+    /**
+     * The current value (returned on next().
+     */
     private Pair<ByteArray, Versioned<byte[]>> value;
+    /**
+     * The data object associated with the current value.
+     */
     private List<Versioned<byte[]>> data;
-    private KDNode<List<Versioned<byte[]>>> lastPosition;
-    private Pair<ByteArray, Versioned<byte[]>> lastValue;
-    private List<Versioned<byte[]>> lastData;
+    /**
+     * The position of the next pair value in data.
+     */
     private int pos;
+    /**
+     * The position used for the last next() call.
+     */
+    private KDNode<List<Versioned<byte[]>>> lastPosition;
+    /**
+     * The value used for the last next() call.
+     */
+    private Pair<ByteArray, Versioned<byte[]>> lastValue;
+    /**
+     * The data used for the last next() call.
+     */
+    private List<Versioned<byte[]>> lastData;
 
+    /**
+     * Create a new Iterator for a given tree.
+     * 
+     * @param root The tree root.
+     */
     public KDTreeIterator(KDNode<List<Versioned<byte[]>>> root) {
+        if(root.getParent() != null) {
+            throw new IllegalArgumentException("KDTreeIterator needs a tree root");
+        }
         position = root;
     }
 
+    /**
+     * Close the current iterator, releases all pointers.
+     */
     @Override
     public void close() {
         position = null;
@@ -31,11 +66,22 @@ public class KDTreeIterator implements ClosableIterator<Pair<ByteArray, Versione
         lastData = null;
     }
 
+    /**
+     * Ask for the existence of a next element.
+     * 
+     * @return true if there is at least one more element.
+     */
     @Override
     public boolean hasNext() {
         return value != null;
     }
 
+    /**
+     * Get the next element. Note that this method will immediately start to
+     * search for another next element and cache it.
+     * 
+     * @return The next key/version&lt;value&gt; pair
+     */
     @Override
     public Pair<ByteArray, Versioned<byte[]>> next() {
         lastValue = value;
@@ -45,6 +91,11 @@ public class KDTreeIterator implements ClosableIterator<Pair<ByteArray, Versione
         return lastValue;
     }
 
+    /**
+     * Search for the next element in the tree.
+     * 
+     * @param startpos The start position within the child array.
+     */
     private void scanNext(final int startpos) {
         data = position.getData();
         if(data != null) {
@@ -65,7 +116,9 @@ public class KDTreeIterator implements ClosableIterator<Pair<ByteArray, Versione
             }
             position = child;
             scanNext(0);
-            return;
+            if(value != lastValue) {
+                return;
+            }
         }
         if(position.getParent() == null) {
             position = null;
@@ -80,6 +133,9 @@ public class KDTreeIterator implements ClosableIterator<Pair<ByteArray, Versione
         scanNext(lastpos + 1);
     }
 
+    /**
+     * Remove the last returned element.
+     */
     @Override
     public void remove() {
         if(lastData == null) {
